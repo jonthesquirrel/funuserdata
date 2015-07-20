1bcd2cd0b486cafc9291caf26a3d45cbc376f810
Jonathan Herman <jdh11235@gmail.com>
2015-07-20 12:04:02 -0500

    working json stats logger

--- keylogger/kl.js
+++ keylogger/kl.js
@@ -3,13 +3,25 @@ var kl = {};
 kl.log = [];
 kl.stats = {};
 
-kl.handler = function (e) {
-  console.log(e);
-  console.log(e.charCode);
-  console.log(String.fromCharCode(e.charCode));
+kl.handler = function(event) {
+  var char = String.fromCharCode(event.charCode);
+  kl.update(char);
 };
 
-kl.init = function () {
+kl.update = function(char) {
+  kl.log.push(char);
+
+  if (kl.stats[char]) {
+    kl.stats[char]++;
+  } else {
+    kl.stats[char] = 1;
+  }
+
+  kl.$log.innerHTML = JSON.stringify(kl.log);
+  kl.$stats.innerHTML = JSON.stringify(kl.stats);
+};
+
+kl.init = function() {
   kl.$input = $id('input');
   kl.$log = $id('log');
   kl.$stats = $id('stats');
